f8a53c4981ab0eb14f3142d195c3e57c38b0343e
Neil Jagtiani <nkjchamp25@gmail.com>
2016-02-09 23:22:41 +0000

    part 1 done

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -9,8 +9,75 @@ class HangpersonGame
   # end
   
   def initialize(word)
+    word.downcase!
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  def word
+    @word
+  end
+
+  def word=(new_word)
+    @word = new_word
+  end
+  
+  def guesses
+    @guesses
+  end
+  
+  def wrong_guesses
+    @wrong_guesses
+  end
+
+  def guess(letter)
+    raise ArgumentError.new("Guess was nil") if letter.nil?
+    letter.downcase!
+    raise ArgumentError.new("Guess was empty") if letter.length==0
+    raise ArgumentError.new("Guess was not a letter") unless ('a'..'z').include?(letter)
+    if @wrong_guesses.include? letter
+      return false
+    elsif @guesses.include? letter
+      return false
+    else
+      if @word.include? letter
+        @guesses << letter
+      else 
+        @wrong_guesses << letter
+      return true
+      end
+    end
+  end
+  
+  def word_with_guesses
+    guessed_word = ""
+    word_array = @word.split('')
+    word_array.each do |l|
+      if @guesses.include? l
+        guessed_word << l
+      else
+        guessed_word << '-'
+      end
+    end
+    guessed_word
+  end
+  
+  def check_win_or_lose
+    # total_guesses = @guesses.length + @wrong_guesses.length
+    if @wrong_guesses.length >= 7
+      return :lose
+    else
+      if self.word_with_guesses.eql? @word
+        return :win
+      else
+        return :play
+      end
+    end
+  end
+  
+    
+  
 
   def self.get_random_word
     require 'uri'
